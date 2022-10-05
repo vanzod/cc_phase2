d37390eec75ae24cefe1611c35c8601d2ea77fdc
Davide Vanzo <davide.vanzo@microsoft.com>
2022-10-05 14:47:06 -0500

    Moved enroot config from Slurm prolog to cluster-init

--- CC_Projects/cc_slurm_pyxis_enroot/specs/default/cluster-init/files/prolog.sh
+++ CC_Projects/cc_slurm_pyxis_enroot/specs/default/cluster-init/files/prolog.sh
@@ -3,24 +3,6 @@
 # Make sure /mnt/resource is mounted
 mount | grep resource || mount /mnt/resource
 
-## Enroot support
-# Exit if enroot is not in the image
-[ -d /etc/enroot ] || exit 0
-
-mkdir -pv /run/enroot /mnt/resource/{enroot-cache,enroot-data,enroot-temp}
-chmod -v 777 /run/enroot /mnt/resource/{enroot-cache,enroot-data,enroot-temp}
-
-ENROOT_CONF=/etc/enroot/enroot.conf
-egrep '^ENROOT_RUNTIME_PATH' ${ENROOT_CONF} || echo 'ENROOT_RUNTIME_PATH /run/enroot/user-$(id -u)' >> ${ENROOT_CONF}
-egrep '^ENROOT_CACHE_PATH' ${ENROOT_CONF} || echo 'ENROOT_CACHE_PATH /mnt/resource/enroot-cache/user-$(id -u)' >> ${ENROOT_CONF}
-egrep '^ENROOT_DATA_PATH' ${ENROOT_CONF} || echo 'ENROOT_DATA_PATH /mnt/resource/enroot-data/user-$(id -u)' >> ${ENROOT_CONF}
-egrep '^ENROOT_TEMP_PATH' ${ENROOT_CONF} || echo 'ENROOT_TEMP_PATH /mnt/resource/enroot-temp' >> ${ENROOT_CONF}
-egrep '^ENROOT_SQUASH_OPTIONS' ${ENROOT_CONF} || echo 'ENROOT_SQUASH_OPTIONS -noI -noD -noF -noX -no-duplicates' >> ${ENROOT_CONF}
-egrep '^ENROOT_MOUNT_HOME' ${ENROOT_CONF} || echo 'ENROOT_MOUNT_HOME n' >> ${ENROOT_CONF}
-egrep '^ENROOT_RESTRICT_DEV' ${ENROOT_CONF} || echo 'ENROOT_RESTRICT_DEV y' >> ${ENROOT_CONF}
-egrep '^ENROOT_ROOTFS_WRITABLE' ${ENROOT_CONF} || echo 'ENROOT_ROOTFS_WRITABLE y' >> ${ENROOT_CONF}
-egrep '^MELLANOX_VISIBLE_DEVICES' ${ENROOT_CONF} || echo 'MELLANOX_VISIBLE_DEVICES all' >> ${ENROOT_CONF}
-
 # Exit if no NVIDIA devices detected
 [ -c /dev/nvidia0 ] || exit 0
 
@@ -40,6 +22,5 @@ if [ ! -c /dev/nvidia-uvm ]; then
         exit 1
     fi
 fi
-
 
 exit 0
