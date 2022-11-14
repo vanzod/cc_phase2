40a263b73035f2d78b6c3347e52dd940306ac89c
Davide Vanzo <davide.vanzo@microsoft.com>
2022-11-14 10:55:24 -0600

    Added Slurm 22.05.3 support

--- CC_Projects/cc_slurmrestd_ubuntu1804/specs/default/cluster-init/files/install_slurmrestd.sh
+++ CC_Projects/cc_slurmrestd_ubuntu1804/specs/default/cluster-init/files/install_slurmrestd.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+SLURM_VERSION=$(apt list --installed | awk '/slurm\// {print $2}')
+
 apt update
 apt install -y libhttp-parser-dev libjwt-dev
 dpkg -i ${SCRIPT_DIR}/libjson-c2_0.11-4ubuntu2_amd64.deb
-dpkg -i ${SCRIPT_DIR}/slurm-slurmrestd_20.11.7-1_amd64.deb
-dpkg -i ${SCRIPT_DIR}/slurm_20.11.7-1_amd64.deb
+dpkg -i ${SCRIPT_DIR}/slurm-slurmrestd_${SLURM_VERSION}_amd64.deb
+dpkg -i ${SCRIPT_DIR}/slurm_${SLURM_VERSION}_amd64.deb
